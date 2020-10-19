45984a3671848c1a2fa78d7f1ac76af6f4cd1ade
Disha167 <khattridisha14@gmail.com>
2020-10-20 02:04:09 +0530

    Create letter case permutation.cpp

---
+++ letter case permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void permutation(string ip, string op,vector<string>&vec)
+{
+    if(ip.length()==0)
+    {
+        vector<string>::iterator it=find(vec.begin(),vec.end(),op);
+        if(it==vec.end())
+          vec.push_back(op);   
+        return;
+    }
+    string op1=op;
+    string op2=op;
+    if(isdigit(ip[0]))
+    {
+        op1.push_back(ip[0]);
+        op2.push_back(ip[0]);
+    }
+    else
+    {
+        op1.push_back(tolower(ip[0]));
+        op2.push_back(toupper(ip[0]));
+    }
+    ip.erase(ip.begin()+0);
+    permutation(ip,op1,vec);
+    permutation(ip,op2,vec);
+    return;
+}
+    vector<string> letterCasePermutation(string S) {
+        string ip=S;
+        string op="";
+        vector<string>vec;
+        permutation(ip,op,vec);
+        return vec;   
+        
+    }
+};
